50cd64d71334b39e14877f44c70fbf6018835083
viniciusrovigomedeiros <viniciusrovigo9@gmail.com>
2022-09-13 11:21:48 -0300

    feat: componentization done

--- lib/portifolio/view/movements_page/movments_page.dart
+++ lib/portifolio/view/movements_page/movments_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/body_home.dart';
-import '../../shered/botton_navigation_bar.dart';
+import '../../widgets/body_home.dart';
+import '../../../shered/botton_navigation_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
