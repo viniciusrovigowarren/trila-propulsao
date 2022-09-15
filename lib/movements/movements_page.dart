1457e19f222d119b6edcd5b1cc1aa95f5a8219b9
viniciusrovigomedeiros <viniciusrovigo9@gmail.com>
2022-09-15 15:16:54 -0300

    feat: implementation back arrow

--- lib/movements/movements_page.dart
+++ lib/movements/movements_page.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trilha_propulsao/portifolio/view/portifolio.dart';
 
+import '../portifolio/provider/provider.dart';
 import '../shared/widgets/botton_navigation_bar.dart';
 
-class MovementsPage extends StatelessWidget {
+class MovementsPage extends HookConsumerWidget {
   const MovementsPage({Key? key}) : super(key: key);
 
   static const route = '/moviments-page';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pageIndex = ref.watch(pageIndexProvider.state);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          leading: const Icon(Icons.arrow_back_ios_new_outlined,
-              color: Colors.black),
+          leading: IconButton(
+            onPressed: () {
+              pageIndex.state = 1;
+              Navigator.pushNamed(context, PortifolioPage.route);
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_outlined,
+                color: Colors.black),
+          ),
           backgroundColor: Colors.white,
           title: const Text(
             'Movimentações',
