815e1bc1a09629ff74c369bb2636ef55ac3bd077
viniciusrovigomedeiros <viniciusrovigo9@gmail.com>
2022-09-19 11:46:42 -0300

    refact: providers

--- lib/details/provider/provider.dart
+++ lib/details/provider/provider.dart
@@ -1,30 +1,5 @@
-import 'package:decimal/decimal.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import '../../portifolio/model/coin_model.dart';
 
 final timeFrameProvider = StateProvider<int>(
   (ref) => 5,
 );
-
-var graphAxisProvider = StateProvider((ref) => 1);
-
-final detailsAssetProvider =
-    StateNotifierProvider<DetailsAssetNotifier, CoinModel>(
-  (ref) => DetailsAssetNotifier(),
-);
-
-class DetailsAssetNotifier extends StateNotifier<CoinModel> {
-  DetailsAssetNotifier()
-      : super(
-          CoinModel(
-            coinBalance: Decimal.parse('1'),
-            currentPrice: Decimal.parse('1'),
-            iconCoin: '',
-            nameCoin: '',
-            prices: [],
-            ticker: '',
-            variation: 1.2,
-          ),
-        );
-}
